0ec333ed54ed10ecb51759cd5134ffa4c229070f
Zahra Dehghanipour <96370252+zahradehghanipour@users.noreply.github.com>
2025-12-23 00:40:45 +0000

    slurms

--- ab-crown/complete_verifier/input_split/sanity_check.py
+++ ab-crown/complete_verifier/input_split/sanity_check.py
@@ -12,7 +12,7 @@
 ##        contained in the LICENCE file in this directory.             ##
 ##                                                                     ##
 #########################################################################
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from os import makedirs
 from math import ceil
 from numpy import ndarray
@@ -41,21 +41,21 @@ def save_sanity_check_graphs(
         warn("'save_sanity_check_graphs' was called but benchmark_name not given. Will skip creating graphs.")
         return
 
-    iterations, features = global_lbs.shape
-    grid_division = 2 if features > 1 else 1
-    plt.figure(figsize=(16, 12))
-    print(f"Saving graphs...")
-    rows = ceil(features / grid_division)
-    for i in range(features):
-        plt.subplot(rows, grid_division, i + 1)
-        best_lb = global_lbs[:, i].min()
-        plt.plot(global_lbs[:, i])
-        plt.grid()
-        plt.title(f"Convergence for feature {i}\nBest (rhs - lb): {best_lb}")
-        plt.xlabel("Iterations")
-        plt.ylabel(f"rhs[{i}] - global_lb[{i}]")
-        plt.yscale('log')
-    save_dir = f"../sanity_check_outputs/{dir_timestamp}"
-    makedirs(save_dir, exist_ok=True)
-    plt.savefig(
-        save_dir + f"/benchmark_{benchmark_name}_vnnlib_id_{vnnlib_id}_property_{property_idx}_sanity_check_graphs.png")
+    # iterations, features = global_lbs.shape
+    # grid_division = 2 if features > 1 else 1
+    # plt.figure(figsize=(16, 12))
+    # print(f"Saving graphs...")
+    # rows = ceil(features / grid_division)
+    # for i in range(features):
+    #     plt.subplot(rows, grid_division, i + 1)
+    #     best_lb = global_lbs[:, i].min()
+    #     plt.plot(global_lbs[:, i])
+    #     plt.grid()
+    #     plt.title(f"Convergence for feature {i}\nBest (rhs - lb): {best_lb}")
+    #     plt.xlabel("Iterations")
+    #     plt.ylabel(f"rhs[{i}] - global_lb[{i}]")
+    #     plt.yscale('log')
+    # save_dir = f"../sanity_check_outputs/{dir_timestamp}"
+    # makedirs(save_dir, exist_ok=True)
+    # plt.savefig(
+    #     save_dir + f"/benchmark_{benchmark_name}_vnnlib_id_{vnnlib_id}_property_{property_idx}_sanity_check_graphs.png")
